9a7f694d252631e3e5c3048168fa45055201314f
AlmironLeandro <leandroealmiron@hotmail.com>
2021-09-14 03:18:21 -0300

    Servicio de login mejorado,manejo de error, navbar con su respectivo usuario

--- app/src/Components/Login/Login.jsx
+++ app/src/Components/Login/Login.jsx
@@ -1,79 +1,85 @@
-import React,{useState,useEffect} from 'react';
-import {Form, Button,Col,Row} from 'react-bootstrap'
-import {useHistory } from 'react-router-dom';
-import {login} from '../../function/pantallaLogin'
-import {serviceLogin} from '../../Servicios/UsuariosServicio'
-import {traerDoc} from '../../Servicios/Entregables'
+import React, { useState, useEffect } from 'react';
+import { Form, Button, Col, Row } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom';
+import { login } from '../../function/pantallaLogin'
+import { serviceLogin } from '../../Servicios/UsuariosServicio'
+import { traerDoc } from '../../Servicios/Entregables'
 
 
-const Login = () =>
-{
+const Login = () => {
   const estilo = {
     display: 'flex',
     justifyContent: 'center',
-    alignItems:'center',
-    height:'90vh'
+    alignItems: 'center',
+    height: '90vh'
   }
   useEffect(() => {
-    traerDoc()
+
   }, [])
-  const [usuario,setUsuario]=useState({dni:'',pass:''})
+  const [usuario, setUsuario] = useState({ dni: '', pass: '' })
+  const [error, setError] = useState(false)
   const history = useHistory();
 
-  const validarIngreso = async  (e)=>
-  {
+  const validarIngreso = async (e) => {
     e.preventDefault();
-    await serviceLogin(usuario.dni,usuario.pass).then(res =>
-    login(res.token,res.idRol,res.id,res.nombre))
-    history.push(`/Usuario/${localStorage.getItem('idRol')}`)  
-    
+    const res = await serviceLogin(usuario.dni, usuario.pass)
+    if (res) {
+      setError(false)
+      login(res.token, res.idRol, res.id, res.nombre)
+      history.push(`/Usuario/${res.idRol}`)
+    }
+    else {
+      setError(true)
+    }
   }
- //HOLAAAAAAAAAAAAAAA
+
   const handleChange = (e) => {
     setUsuario({
-        ...usuario,
-        [e.target.name]: e.target.value,
+      ...usuario,
+      [e.target.name]: e.target.value,
     }
     )
-};
-  
+  };
 
- return (
-  <div style={estilo} > 
-    <div>
-  
-      <Form className="login">
-            <Col  xs={{ order: 12 }}>
-              <Row className="rowLogin number">
-                <Form.Group controlId="formBasicNumber">
-                  <Form.Label  >Documento</Form.Label>
-                  <Form.Control value={usuario.dni} name="dni" type="number" placeholder="Ingresar documento" onChange={handleChange}/>
-                </Form.Group>
-              </Row>
-              <Row className="rowLogin">
-                <Form.Group controlId="formBasicPassword" >
-                  <Form.Label>Contraseña</Form.Label>
-                  <Form.Control onChange={handleChange} name="pass" value={usuario.pass} type="password" placeholder="Ingresar contraseña" />
-                </Form.Group>
-              </Row >
-              {/* <Link style={{textDecoration: 'none'}} to={"/Usuario/2"}> */}
-                  <Row className="d-flex justify-content-center rowLogin">
-                    <Button className=" btn btn-success buttonSeccion " 
-                      type="submit"  onClick={validarIngreso}  > 
-                        Iniciar sesion
-                    </Button>
-              
-                  </Row>
-                {/* </Link>  */}
-                
-              
-                 
-            </Col>
-      </Form>
-       
-    </div>  
-  </div>
-    )
-  }
+
+  return (
+    <div style={estilo} >
+      <div>
+
+        <Form className="login">
+
+          <Col xs={{ order: 12 }}>
+            <Row className="rowLogin number">
+              {error ? <p style={{ color: 'red', textAlign: 'center', fontSize: '15px' }}> El documento y/o contraseña no son validos </p> : null}
+              <Form.Group controlId="formBasicNumber">
+                <Form.Label  >Documento</Form.Label>
+                <Form.Control value={usuario.dni} name="dni" type="number" placeholder="Ingresar documento" onChange={handleChange} />
+              </Form.Group>
+            </Row>
+            <Row className="rowLogin">
+              <Form.Group controlId="formBasicPassword" >
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control onChange={handleChange} name="pass" value={usuario.pass} type="password" placeholder="Ingresar contraseña" />
+              </Form.Group>
+            </Row >
+            {/* <Link style={{textDecoration: 'none'}} to={"/Usuario/2"}> */}
+            <Row className="d-flex justify-content-center rowLogin">
+              <Button className=" btn btn-success buttonSeccion "
+                type="submit" onClick={validarIngreso}  >
+                Iniciar sesion
+              </Button>
+
+            </Row>
+            {/* </Link>  */}
+
+
+
+          </Col>
+        </Form>
+
+      </div>
+    </div>
+  )
+}
 
 export default Login;
--- app/src/Servicios/UsuariosServicio.jsx
+++ app/src/Servicios/UsuariosServicio.jsx
@@ -34,12 +34,19 @@ export const serviceLogin = async (dni, pass) => {
             return response.data.data;
         }
     }
+<<<<<<< HEAD
     catch (error) {
         console.error(error);
+=======
+    catch {
+        console.log('error');
+        // alert('Usuario y/o contraseña incorrecta');
+>>>>>>> af9483d (Servicio de login mejorado,manejo de error, navbar con su respectivo usuario)
     }
 };
 
 export const traerUsuarios = async () => {
+<<<<<<< HEAD
     try {
         const cliente = newSecureClient();
         const response = await cliente.get('/usuarios')
@@ -67,6 +74,52 @@ export const insertarUsuario = async (nombre, apellido, dni, email, pass, idRol,
     catch (error) {
         console.error(error);
     }
+=======
+    const cliente = newSecureClient();
+
+    return await cliente.get('/usuarios')
+        .then(response => {
+            const alumnosTodas = response.data.data
+            return alumnosTodas
+        })
+        .catch(error => {
+            console.log(error)
+        }
+        )
+}
+// const response = await post(...)
+// if (response.status !== 200) { ... }
+// Try catch --- async && await || then()
+
+export const insertarUsuario = (nombre, apellido, dni, email, pass, idRol, idCarrera) => {
+    const cliente = newSecureClient();
+    return cliente.post(`/usuarios`, {
+        nombre, apellido, dni, email, pass, idRol, idCarrera
+    })
+        .then(response => {
+            if (response.status === 200) {
+                return response.data.data;
+            } else {
+                console.log('error');
+                throw new Error('No se pudo agregar el usuario');
+            }
+        }).catch((e) => { console.log(e); throw new Error('No se pudo agregar el usuario'); });
+};
+
+export const eliminarUsuario = (id) => {
+    const cliente = newSecureClient();
+    console.log(`es para el id: ${id}`)
+    return cliente.delete(`/usuarios/${id}`, {
+
+    })
+        .then(response => {
+            if (response.status === 200) {
+                return;
+            } else {
+                throw new Error('No se pudo eliminar el usuario');
+            }
+        }).catch((e) => { throw new Error('No se pudo eliminar el usuario'); });
+>>>>>>> af9483d (Servicio de login mejorado,manejo de error, navbar con su respectivo usuario)
 };
 
 export const eliminarUsuario = async (id) => {
@@ -75,6 +128,7 @@ export const eliminarUsuario = async (id) => {
         const response = await cliente.delete(`/usuarios/${id}`, {
         })
 
+<<<<<<< HEAD
         if (response.status === 200) {
             return;
         }
@@ -82,6 +136,24 @@ export const eliminarUsuario = async (id) => {
     catch (error) {
         console.error(error);
     }
+=======
+
+export const editarUsuario = (ID, nombre, apellido, dni, email, pass, idRol, idCarrera) => {
+    const cliente = newSecureClient();
+
+    return cliente.put(`/usuarios/${ID}`, {
+        nombre, apellido, dni, email, pass, idRol, idCarrera
+    })
+        .then(response => {
+            if (response.status === 200) {
+                //console.log("llega al put")
+                return response.data.data;
+
+            } else {
+                throw new Error('No se pudo modificar el usuario');
+            }
+        }).catch((e) => { throw new Error('No se pudo mod el usuario'); });
+>>>>>>> af9483d (Servicio de login mejorado,manejo de error, navbar con su respectivo usuario)
 };
 
 export const editarUsuario = async (ID, nombre, apellido, dni, email, idRol, idCarrera) => {
@@ -108,9 +180,15 @@ export const buscarUsuario = async (id) => {
         }
     }
     catch (error) {
+<<<<<<< HEAD
         console.error(error);
     }
 
 };
 
+=======
+        alert(error)
+    }
+}
+>>>>>>> af9483d (Servicio de login mejorado,manejo de error, navbar con su respectivo usuario)
 
