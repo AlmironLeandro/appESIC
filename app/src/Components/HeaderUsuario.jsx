af9483de4ec110585061a0e149c8ec5dc6c62f0a
AlmironLeandro <leandroealmiron@hotmail.com>
2021-09-14 03:18:21 -0300

    Servicio de login mejorado,manejo de error, navbar con su respectivo usuario

--- app/src/Components/HeaderUsuario.jsx
+++ app/src/Components/HeaderUsuario.jsx
@@ -1,70 +1,73 @@
 import { Dropdown } from 'react-bootstrap'
 import Logo from '../Images/imgg.png'
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
-import { Button } from 'bootstrap';
 import { logout } from '../function/pantallaLogin'
+import { buscarUsuario } from '../Servicios/UsuariosServicio'
 
 
 const NavDeUsuarios = () => {
 
-    const styleLogo =
+    const styles =
     {
-        display: 'inline-flex',
-        margin: '1%',
-        height: '45px',
-        opacity: '0.6'
+        styleLogo:
+        {
+            display: 'inline-flex',
+            margin: '1%',
+            height: '45px',
+            opacity: '0.6'
+        },
+        foto:
+        {
+            width: '70px',
+            height: '65px'
+        }
     }
-
-    //FUNCION ASYNC QUE CONECTA CON API CON EL NOMBRE DE USUARIO.
-    const nombre = "Diego Migliorino"
-
-    const foto =
-    {
-        width: '70px',
-        height: '65px'
-
-
-    }
+    const [nombre, setNombre] = useState()
+    const [apellido, setApellido] = useState()
+    useEffect(async () => {
+        const res = await buscarUsuario(localStorage.getItem("id"))
+        console.log(res)
+        console.log(res)
+        setNombre(res.usuario.nombre)
+        setApellido(res.usuario.apellido)
+        console.log(nombre)
+        console.log(apellido)
+    }, [])
     const history = useHistory();
-    const redirigir = async ()=>{
+    const redirigir = async () => {
         await logout()
-        history.push("/login") 
- }
+        history.push("/login")
+    }
     return (
         <Fragment>
             <div className="header">
                 <div style={{ width: '150px' }}>
-                    <img alt='' src={Logo} style={styleLogo}></img>
+                    <img alt='' src={Logo} style={styles.styleLogo}></img>
                     <h2 style={{ display: 'inline', fontSize: '25px' }} > ESIC</h2>
                 </div>
 
                 <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-
                     <Dropdown  >
                         <Dropdown.Toggle bsPrefix='dropdown' as="Button" id="dropdown-basic">
-                            {nombre}
+                            {nombre && apellido ? nombre + ' ' + apellido : ''}
                         </Dropdown.Toggle>
-
                         <Dropdown.Menu >
                             {/* TODO: COMPLETAR LA PANTALLA DE PERFIL... */}
                             <Dropdown.Item href="#/action-1">
                                 <Link style={{ textDecoration: 'none' }} to={"/InicioTutor"}>Ver perfil</Link>
                             </Dropdown.Item>
-
                             <Dropdown.Item href="#/action-2">
                                 <Link style={{ textDecoration: 'none' }} to={"/EditarPerfil"}>Editar perfil</Link>
                             </Dropdown.Item>
-
-                            <Dropdown.Item onClick={ redirigir}>
+                            <Dropdown.Item onClick={redirigir}>
                                 Cerrar sesion
                             </Dropdown.Item>
-
                         </Dropdown.Menu>
                     </Dropdown>
                     <div>
-                        <Avatar style={foto}>{nombre[0] + nombre[6]}</Avatar>
+                        {nombre && apellido ? <Avatar style={styles.foto}>{nombre[0] + apellido[0]}</Avatar> : ''}
                     </div>
                 </div>
             </div>
--- app/src/Servicios/UsuariosServicio.jsx
+++ app/src/Servicios/UsuariosServicio.jsx
@@ -24,57 +24,59 @@ const nuevoCliente = () => {
     });
 }
 
-export const serviceLogin =  async (dni, pass) => {
-    const cliente = nuevoCliente();
-    return  cliente.post(`/usuarios/login`, {
-     dni ,pass
-  })
-      .then(response => {
-          if (response.status === 200) {
-              return response.data.data;
-          } else {
-              console.log('error');
-              throw new Error('Usuario y/o contraseña incorrecta');
-          }
-      }).catch((e) => { console.log(e); throw new Error('Usuario y/o contraseña incorrecta'); });
+export const serviceLogin = async (dni, pass) => {
+    try {
+        const cliente = nuevoCliente();
+        const response = await cliente.post(`/usuarios/login`, {
+            dni, pass
+        })
+        if (response.status === 200) {
+            return response.data.data;
+        }
+    }
+    catch {
+        console.log('error');
+        // alert('Usuario y/o contraseña incorrecta');
+    }
 };
 
-export const traerUsuarios = async()=>{
+export const traerUsuarios = async () => {
     const cliente = newSecureClient();
 
-    return await  cliente.get('/usuarios')
-    .then(response => {
-        const alumnosTodas = response.data.data
-        return alumnosTodas  
-    })
-    .catch(error => {
-        console.log(error)
+    return await cliente.get('/usuarios')
+        .then(response => {
+            const alumnosTodas = response.data.data
+            return alumnosTodas
+        })
+        .catch(error => {
+            console.log(error)
+        }
+        )
 }
-)}
 // const response = await post(...)
 // if (response.status !== 200) { ... }
 // Try catch --- async && await || then()
 
-export const insertarUsuario =  (nombre, apellido, dni, email, pass, idRol, idCarrera) => {
-    const cliente  = newSecureClient();
-    return  cliente.post(`/usuarios`, {
-    nombre, apellido, dni, email, pass, idRol, idCarrera
-  })
-      .then(response => {
-          if (response.status === 200) {
-              return response.data.data;
-          } else {
-              console.log('error');
-              throw new Error('No se pudo agregar el usuario');
-          }
-      }).catch((e) => { console.log(e); throw new Error('No se pudo agregar el usuario'); });
+export const insertarUsuario = (nombre, apellido, dni, email, pass, idRol, idCarrera) => {
+    const cliente = newSecureClient();
+    return cliente.post(`/usuarios`, {
+        nombre, apellido, dni, email, pass, idRol, idCarrera
+    })
+        .then(response => {
+            if (response.status === 200) {
+                return response.data.data;
+            } else {
+                console.log('error');
+                throw new Error('No se pudo agregar el usuario');
+            }
+        }).catch((e) => { console.log(e); throw new Error('No se pudo agregar el usuario'); });
 };
 
-export const eliminarUsuario =  (id) => {
+export const eliminarUsuario = (id) => {
     const cliente = newSecureClient();
     console.log(`es para el id: ${id}`)
     return cliente.delete(`/usuarios/${id}`, {
-       
+
     })
         .then(response => {
             if (response.status === 200) {
@@ -87,37 +89,33 @@ export const eliminarUsuario =  (id) => {
 
 
 
-export const editarUsuario =  (ID, nombre, apellido, dni, email, pass, idRol, idCarrera) => {
+export const editarUsuario = (ID, nombre, apellido, dni, email, pass, idRol, idCarrera) => {
     const cliente = newSecureClient();
-    
-    return  cliente.put(`/usuarios/${ID}`, {
-       nombre, apellido, dni, email, pass, idRol, idCarrera
+
+    return cliente.put(`/usuarios/${ID}`, {
+        nombre, apellido, dni, email, pass, idRol, idCarrera
     })
         .then(response => {
             if (response.status === 200) {
                 //console.log("llega al put")
                 return response.data.data;
-                
-            } else{               
+
+            } else {
                 throw new Error('No se pudo modificar el usuario');
             }
         }).catch((e) => { throw new Error('No se pudo mod el usuario'); });
 };
 
 export const buscarUsuario = async (id) => {
-    const cliente = newSecureClient();
-    return await cliente.get(`/usuarios/${id}`) 
-    .then(response => {
+    try {
+        const cliente = newSecureClient();
+        const response = await cliente.get(`/usuarios/${id}`)
         if (response.status === 200) {
-            // console.log((response.data))
-          
             return response.data;
-            } else {
-                
-                throw new Error('No se pudo listar el usuario');
-            }
-        }).catch((e) => { throw new Error('No se pudo listar usuario'); });
-};
+        }
+    }
+    catch (error) {
+        alert(error)
+    }
+}
 
-
-    
